d424c312ebcbdb71d54b9aa1efc87ba078361e4b
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-16 13:35:07 +0530

    updated packages

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, it, expect } from '@testing-library/react'
-import App from './App'
-
-it('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
